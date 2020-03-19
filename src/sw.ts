ad62eae00bae96164a7727dc518b0e5ab4deba35
Kaixinguo <kaixinguo@kaixinguo.site>
2020-03-19 15:28:56 +0800

    Add exception handler to sw.js to provide a user-friendly error page

--- src/sw.ts
+++ src/sw.ts
@@ -11,12 +11,12 @@ self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('fetch', e => e.respondWith(handleFetch(e)));
 
 // ----- Fetch Handlers ----- //
-function handleFetch(event: FetchEvent) {
+function handleFetch(event: FetchEvent): Promise<any> {
   const request = event.request;
 
   if (request.url.startsWith(location.origin)) {
     const path = request.url.substr(location.origin.length);
-    if (path.startsWith('/proxy/hook.js') || path.startsWith('/proxy/add') || path.startsWith('/sockjs-node/')) {
+    if (path.startsWith('/proxy/hook.js') || path.startsWith('/sockjs-node/')) {
       return fetch(request);
     } else if (path.startsWith('/proxy/')) {
       return handleFetch_SamePath(event);
@@ -24,10 +24,10 @@ function handleFetch(event: FetchEvent) {
       return handleFetch_SameOrigin(event);
     }
   } else {
-    return handleRequest(event, request.url);
+    return beforeRequest(event, request.url);
   }
 }
-function handleFetch_SamePath(event: FetchEvent) {
+function handleFetch_SamePath(event: FetchEvent): Promise<any> {
   const request = event.request;
 
   // get encodeUrl
@@ -47,26 +47,24 @@ function handleFetch_SamePath(event: FetchEvent) {
   } catch (e) {
     return getClientBase(event.clientId).then(base => {
       const urlStr = proxyService.absolute(encodeUrl, base);
-      return handleRequest(event, urlStr);
+      return beforeRequest(event, urlStr);
     });
   }
 
   const base = new URL(url);
-  return fetch(request)
-    .then(res => handleResponse(event, res, base))
-    .catch(() => makeRes('fetch failed', 504));
+  return afterRequest(event, request, base);
 }
-function handleFetch_SameOrigin(event: FetchEvent) {
+function handleFetch_SameOrigin(event: FetchEvent): Promise<any> {
   return getClientBase(event.clientId).then(base => {
     const origin = base.origin;
     const path = event.request.url.substr(location.origin.length);
     const urlStr = origin + path;
-    return handleRequest(event, urlStr);
+    return beforeRequest(event, urlStr);
   });
 }
 
 // ----- Request & Response ----- //
-function handleRequest(event: FetchEvent, url: string) {
+function beforeRequest(event: FetchEvent, url: string) {
   const request = event.request;
   const base = new URL(url);
   const proxyUrl = proxyService.proxy(url, request.destination, base);
@@ -76,9 +74,14 @@ function handleRequest(event: FetchEvent, url: string) {
     destination: request.destination, url
   });
 
-  return fetch(req)
-    .then(res => handleResponse(event, res, base))
-    .catch(() => makeRes('fetch failed', 504));
+  return afterRequest(event, req, base);
+}
+function afterRequest(event: FetchEvent, request: Request, base: URL) {
+  return fetch(request)
+    .then(res => res.ok ?
+      handleResponse(event, res, base) :
+      handleException(event, res, base))
+    .catch(() => handleException(event, null, base));
 }
 function handleResponse(event: FetchEvent, response: Response, base: URL) {
   const request = event.request;
@@ -87,7 +90,7 @@ function handleResponse(event: FetchEvent, response: Response, base: URL) {
     case 'script':
       return response.text().then(text => {
         const body = text.replace(/location/g, '__location');
-        return makeRes(body, 200, response.headers);
+        return makeResponse(body, 200, response.headers);
       });
 
     case 'document':
@@ -109,7 +112,7 @@ function handleResponse(event: FetchEvent, response: Response, base: URL) {
             + `<script>ORIGIN='${origin}';PATH='${path}';</script>\n`
             + `<script src='/proxy/hook.js'></script>\n`
             + `<meta name='referrer' content='never'>\n`);
-        return makeRes(body, 200, response.headers);
+        return makeResponse(body, 200, response.headers);
       });
 
     case 'style':
@@ -118,7 +121,7 @@ function handleResponse(event: FetchEvent, response: Response, base: URL) {
           /url\s*\(\s*([^\s]*)\s*\)/g,
           (...str) => `url(${proxyService.absolute(str[1], base)})`
         );
-        return makeRes(text, 200, response.headers);
+        return makeResponse(text, 200, response.headers);
       });
 
     default:
@@ -126,12 +129,43 @@ function handleResponse(event: FetchEvent, response: Response, base: URL) {
   }
 }
 
+// ----- Exception Handlers ----- //
+function handleException(event: FetchEvent, response: Response | null, base: URL): Response | Promise<any> {
+  const request = event.request;
+  if (request.destination === 'document') {
+    return showFail(event, response, base);
+  } else {
+    return makeResponse('fetch failed', 504);
+  }
+}
+function showFail(event: FetchEvent, response: Response | null, base: URL) {
+  const request = event.request;
+  const title = response ? response.statusText : 'Network Error';
+  const body = `
+                    <h3>${title}</h3>
+                    <br>Origin URL: ${base.href}
+                    <br>Proxy URL: ${request.url}
+                    ${response?`<br>Status: ${response.status}`:''}
+  `;
+  return handleResponse(event, makeHtmlResponse(title, body), base);
+}
+
 // ----- Utils ----- //
-function makeRes(body: any, status = 200, headers: any = {}) {
+function makeHtmlResponse(title: string, body: string) {
+  return makeResponse(`
+                    <html lang="CN-zh">
+                    <head><title>${title}</title></head>
+                    <body>${body}</body>
+                    </html>
+    `, 200, {
+    'content-type': 'text/html'
+  });
+}
+function makeResponse(body: any, status = 200, headers: any = {}): Response {
   headers['access-control-allow-origin'] = '*';
   return new Response(body, {status, headers});
 }
-function getClientBase(clientId: string) {
+function getClientBase(clientId: string): Promise<URL> {
   return self.clients.get(clientId).then(client => {
     if (!client.base) {
       const encodeUrl = client.url.substr(client.url.lastIndexOf('/') + 1);
